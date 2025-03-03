f34b0f12886c4e325286f95dd28dae86a0198f2a
Mauricio <mauricio_cy@hotmail.com>
2025-03-03 14:29:40 -0300

    adding middlewares for errors handling

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+
+export function errorHandling(
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error instanceof AppError)
+    return res.status(error.statusCode).json({ message: error.message });
+
+  return res.status(500).json({ message: error.message });
+}
